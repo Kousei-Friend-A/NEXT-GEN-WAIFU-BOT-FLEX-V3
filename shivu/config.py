378f46592d7e49512661a2c70bb31b4140969d4d
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-13 15:08:05 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = ["5630057244", "2010819209", "5702598840", "6101457748", "6154972031", "1735664760", "7036005233", "6100011620", "7297953309", "6412447141", "7244871367", "5530116994", "6584789596", "949302414"]
     GROUP_ID = "-1002383369786"
     TOKEN = "7501936092:AAHdgIDKUwMKCffWehEAl1aZ6GiX1zADaUA"
-    mongo_url = "mongodb://root:SzobqGSLSTsJH6fhoPAy4xIl7Sx2vxJX9ilCMflCU1gp2rPggyOfQ7Zsqa8EZ6yS@dsgkso48c4wkkg0cgc44cgco:27017/?directConnection=true"
+    mongo_url = "mongodb+srv://friendakouseimanu:asdfg@cluster0.1trpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg", "https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/32556c77847dff110577c.jpg", "https://telegra.ph//file/0650844fc5db4049959bc.jpg"]
     SUPPORT_CHAT = "Grabbing_Your_WH_Group"
     UPDATE_CHAT = "FLEX_BOTS_NEWS"
